66af0e41127219f7ec36f82f7e311f9eafa3d875
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-03-13 10:44:55 +0200

    setting up tmdb config and endpoints for url

---
+++ server/src/tmdb/tmdb.endpoints.js
@@ -0,0 +1,34 @@
+import tmdbConfig from "./tmdb.config.js";
+
+const tmdbEndPoints = {
+    mediaList: ({mediaType, mediaCategory, page}) =>tmdbConfig.getUrl(
+        `${mediaType}/${mediaCategory}`, page
+    ),
+    mediaDetails: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}`
+    ),
+    mediaGenres: ({mediaType}) =>tmdbConfig.getUrl(
+        `genre/${mediaType}/list`
+    ),
+    mediaCredits: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/credits`
+    ),
+    mediaVideos: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/videos`
+    ),
+    mediaRecomendations: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/recomendations`
+    ),
+    mediaImages: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/images`
+    ),
+    mediaSearch: ({mediaType, query, page}) => tmdbConfig.getUrl(
+        `search/${mediaType}`, {query, page}
+    ),
+    personDetails: ({personId}) => tmdbConfig.getUrl(
+        `person/${personId}`
+    ),
+    personMedias: ({personId}) => tmdbConfig.getUrl(
+        `person/${personId}/combined_credits`
+    ),
+};
