6c8e3b4312d856dfdc73f6cdc0ba2518b548d4c4
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-03-13 10:56:58 +0200

    finishing tmdb api

---
+++ server/src/tmdb/tmdb.api.js
@@ -0,0 +1,33 @@
+import axiosClient from '../axios/axios.client.js';
+import tmdbEndPoints from './tmdb.endpoints.js';
+
+
+const tmdbApi = {
+    mediaList: async ({ mediaType, mediaCategory, page }) => await axiosClient.get(
+        tmdbEndPoints.mediaList({ mediaType, mediaCategory, page })
+    ),
+    mediaDetails: async ({ mediaType, page }) => await axiosClient.get(
+        tmdbEndPoints.mediaDetails({ mediaType, page })
+    ),
+    mediaGenres: async ({ mediaType }) => await axiosClient.get(
+        tmdbEndPoints.mediaGenres({ mediaType })
+    ),
+    mediaCredits: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndPoints.mediaCredits({ mediaType, mediaId })
+    ),
+    mediaVideos: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndPoints.mediaVideos({ mediaType, mediaId })
+    ),
+    mediaImages: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndPoints.mediaImages({ mediaType, mediaId })
+    ),
+    mediaRecomendations: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndPoints.mediaRecomendations({ mediaType, mediaId })
+    ),
+    mediaSearch: async ({ mediaType, query, page }) => await axiosClient.get(
+        tmdbEndPoints.mediaSearch({ mediaType, query, page })
+    ),
+    personMedias: async ({ personId }) => await axiosClient.get(
+        tmdbEndPoints.personMedias({ personId })
+    ),
+}
--- server/src/tmdb/tmdb.endpoints.js
+++ server/src/tmdb/tmdb.endpoints.js
@@ -1,34 +1,34 @@
 import tmdbConfig from "./tmdb.config.js";
 
 const tmdbEndPoints = {
-    mediaList: ({mediaType, mediaCategory, page}) =>tmdbConfig.getUrl(
+    mediaList: ({ mediaType, mediaCategory, page }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaCategory}`, page
     ),
-    mediaDetails: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+    mediaDetails: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}`
     ),
-    mediaGenres: ({mediaType}) =>tmdbConfig.getUrl(
+    mediaGenres: ({ mediaType }) => tmdbConfig.getUrl(
         `genre/${mediaType}/list`
     ),
-    mediaCredits: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+    mediaCredits: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}/credits`
     ),
-    mediaVideos: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+    mediaVideos: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}/videos`
     ),
-    mediaRecomendations: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+    mediaRecomendations: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}/recomendations`
     ),
-    mediaImages: ({mediaType, mediaId}) =>tmdbConfig.getUrl(
+    mediaImages: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}/images`
     ),
-    mediaSearch: ({mediaType, query, page}) => tmdbConfig.getUrl(
-        `search/${mediaType}`, {query, page}
+    mediaSearch: ({ mediaType, query, page }) => tmdbConfig.getUrl(
+        `search/${mediaType}`, { query, page }
     ),
-    personDetails: ({personId}) => tmdbConfig.getUrl(
+    personDetails: ({ personId }) => tmdbConfig.getUrl(
         `person/${personId}`
     ),
-    personMedias: ({personId}) => tmdbConfig.getUrl(
+    personMedias: ({ personId }) => tmdbConfig.getUrl(
         `person/${personId}/combined_credits`
     ),
 };
