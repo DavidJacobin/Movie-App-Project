1742761ec9b1d805ea3b17aff717dbb8a00a19f8
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-03-20 18:51:53 +0200

    light and dark theme config added

---
+++ client/src/configs/theme.config.js
@@ -0,0 +1,51 @@
+import { createTheme} from "@mui/material/styles";
+import { colors } from "@mui/material";
+
+export const themeModes = {
+    dark: 'dark',
+    light: 'light'
+};
+
+const themeConfigs = {
+    custom: ({mode}) => {
+        const customPallette = mode === themeModes.dark ? {
+            primary: {
+                main: '#ff0000',
+                contrastText: '#ffffff'
+            },
+            secondary: {
+                main: '#f44336',
+                contrastText: '#ffffff'
+            },
+            background: {
+                default: '#000000',
+                paper: 'f131313'
+            }
+        } : {
+            primary: {
+                main: '#ff0000',
+                contrastText: '#ffffff'
+            },
+            secondary: {
+                main: '#f44336',
+                contrastText: '#ffffff'
+            },
+            background: {
+                default: colors.grey['100']
+            }
+        }
+        return createTheme({
+            palette: {
+                mode,
+                ...customPallette
+            },
+            components: {
+                MuiButton: {
+                    defaultProps: {disableElevation: true}
+                }
+            }
+        })
+    }
+}
+
+export default themeConfigs;
