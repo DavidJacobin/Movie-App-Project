cd9cf27d1e1add1e722024063cba2bddb3f8a540
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-03-20 19:00:32 +0200

    adding theme provider and toastcontainer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { ThemeProvider } from "@emotion/react";
+import { useSelector } from 'react-redux'
+import themeConfigs from "./configs/theme.config.js";
+import { ToastContainer } from "react-toastify"
 
 const App = () => {
+  const { themeMode } = useSelector((state) => state.themeMode)
   return (
-    <div className="App">
-      App
-    </div>
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+
+    </ThemeProvider>
   );
 }
 
