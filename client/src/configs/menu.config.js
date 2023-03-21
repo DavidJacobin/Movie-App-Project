4adadad3775a0dc0437bcb8df539cd54c7d11241
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-03-21 11:29:46 +0200

    creating menu config

---
+++ client/src/configs/menu.config.js
@@ -0,0 +1,60 @@
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import SlideshowOutlinedIcon from "@mui/icons-material/SlideshowOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import FavoriteBoredrOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import RateReviewOutlinedIcon from "@mui/icons-material/RateReviewOutlined";
+import LockRestOutlinedIcon from "@mui/icons-material/LockResetOutlined";
+
+const main = [
+    {
+        display: "home",
+        path: "/",
+        icon: <HomeOutlinedIcon/>,
+        state: "home" 
+    },
+    {
+        display: "movies",
+        path: "/movie",
+        icon: <SlideshowOutlinedIcon/>,
+        state: "movie" 
+    },
+    {
+        display: "tv series",
+        path: "/tv",
+        icon: <LiveTvOutlinedIcon/>,
+        state: "tvhome" 
+    },
+    {
+        display: "search",
+        path: "/search",
+        icon: <SearchOutlinedIcon/>,
+        state: "search" 
+    },
+];
+
+const user = [
+    {
+        display: "favorites",
+        path: "/favorites",
+        icon: <FavoriteBoredrOutlinedIcon/>,
+        state: "favorites" 
+    },
+    {
+        display: "reviews",
+        path: "/reviews",
+        icon: <RateReviewOutlinedIcon/>,
+        state: "review" 
+    },
+    {
+        display: "password update",
+        path: "/password-update",
+        icon: <LockRestOutlinedIcon/>,
+        state: "password.update" 
+    },
+    
+];
+
+const menuConfig = {user, main};
+
+export default menuConfig;
