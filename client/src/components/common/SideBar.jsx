cacda872d3acd5e3d231635142c5e0cb293ca416
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-03-25 17:59:50 +0200

    adding the sidebar

---
+++ client/src/components/common/SideBar.jsx
@@ -0,0 +1,104 @@
+import { Drawer, List, ListItemButton, ListItemIcon, ListItemText, Stack, Toolbar, Typography } from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import menuConfigs from '../../configs/menu.config';
+import uiConfig from '../../configs/ui.config'
+import Logo from './Logo';
+
+
+const SideBar = ({ open, toggleSidebar }) => {
+    const dispatch = useDispatch();
+
+    const { user } = useSelector((state) => state.user);
+    const { appState } = useSelector((state) => state.appState);
+
+    const sideBarWidth = uiConfig.size.sidebarWidth;
+
+    const drawer = (
+        <>
+            <Toolbar sx={{ paddingY: "20px", color: "text.primary" }}>
+                <Stack width="100%" direction="row" justifyContent="center">
+                    <Logo />
+                </Stack>
+            </Toolbar>
+
+            <List sx={{ paddingX: "30px" }}>
+                <Typography variant='h6' marginBottom="20px">MENU</Typography>
+                {
+                    menuConfigs.main.map((item, index) => (
+                        <ListItemButton
+                            key={index}
+                            sx={{
+                                borderRadius: "10px",
+                                marginY: 1,
+                                backgroundColor: appState.includes(item.state) ? "primary.main" :
+                                    "unset"
+                            }}
+                            component={Link}
+                            to={item.path}
+                            onClick={() => toggleSidebar(false)}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText disableTypography
+                                primary={
+                                    <Typography
+                                        textTransform="uppercase"
+                                    >{item.display}</Typography>
+                                }
+                            />
+                        </ListItemButton>
+                    ))}
+                {user && (
+                    <>
+                        <Typography variant='h6' marginBottom="20px">PERSONAL</Typography>
+                        {
+                            menuConfigs.user.map((item, index) => (
+                                <ListItemButton
+                                    key={index}
+                                    sx={{
+                                        borderRadius: "10px",
+                                        marginY: 1,
+                                        backgroundColor: appState.includes(item.state) ? "primary.main" :
+                                            "unset"
+                                    }}
+                                    component={Link}
+                                    to={item.path}
+                                    onClick={() => toggleSidebar(false)}
+                                >
+                                    <ListItemIcon>{item.icon}</ListItemIcon>
+                                    <ListItemText disableTypography
+                                        primary={
+                                            <Typography
+                                                textTransform="uppercase"
+                                            >{item.display}</Typography>
+                                        }
+                                    />
+                                </ListItemButton>
+                            ))}
+                    </>
+                )}
+
+
+
+            </List>
+        </>
+    )
+
+    return (
+        <Drawer
+            open={open}
+            onClose={() => toggleSidebar(false)}
+            sx={{
+                "& .MuiDrawer-Paper": {
+                    boxSizing: "border-box",
+                    width: sideBarWidth,
+                    borderRight: "0px"
+                }
+            }}
+        >
+            {drawer}
+        </Drawer>
+    )
+}
+
+export default SideBar
--- client/src/configs/ui.config.js
+++ client/src/configs/ui.config.js
@@ -38,10 +38,11 @@ const uiConfig = {
             backgroundColor: "darkgrey",
             backgroundImage: `url(${imgPath})`
         }),
-        size: {
-            sidebarWidth: "300px",
-            contentMaxWidth: "1366px",
-        }
+    
+    },
+    size: {
+        sidebarWidth: "300px",
+        contentMaxWidth: "1366px",
     }
 };
 
