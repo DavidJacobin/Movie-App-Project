9115bd8512517c883e3a7cf65067c1c53f9f4056
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-04-01 11:09:22 +0300

    adding media detals

--- client/src/pages/MediaDetail.jsx
+++ client/src/pages/MediaDetail.jsx
@@ -21,6 +21,8 @@ import ImageHeader from '../components/common/ImageHeader';
 import { setGlobalLoading } from '../redux/features/globalLoadingSlice.js';
 import { setAuthModalOpen } from '../redux/features/authModalSlice.js';
 import { addFavorite, removeFavorite } from '../redux/features/userSlice.js';
+import { color } from '@mui/system';
+import PlayArrow from '@mui/icons-material/PlayArrow';
 ///////
 
 
@@ -44,29 +46,156 @@ const MediaDetail = () => {
         window.scrollTo(0, 0);
         const getMedia = async () => {
             dispatch(setGlobalLoading(true));
-            const { response, err } = await mediaApi.getDetail({mediaType, mediaId});
+            const { response, err } = await mediaApi.getDetail({ mediaType, mediaId });
             dispatch(setGlobalLoading(false));
 
             if (response) {
                 setMedia(response);
                 setIsFavorite(response.isFavorite);
-                setGenres(response.genres.splice(0,2));
+                setGenres(response.genres.splice(0, 2));
             }
 
             if (err) toast.error(err.message);
 
 
         };
-        
+
         getMedia();
 
     }, [mediaType, mediaId, dispatch])
 
 
     return (
-        //<ImageHeader imgPath={tmdbConfigs.backdropPath(media.backdrop_path || media.poster_path)}/>
-        <>DETAILS</>
-        
+
+        media ? (
+
+            <>
+                <ImageHeader imgPath={tmdbConfigs.backdropPath(media.backdrop_path || media.poster_path)} />
+                <Box
+                    sx={{
+                        color: "primary.contrastText",
+                        ...uiConfigs.style.mainContent
+                    }}
+                >
+
+                    <Box
+                        sx={{
+                            marginTop: { xs: '-10rem', md: "-15rem", lg: "-20rem" }
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexDirection: { md: "row", xs: "column" }
+                            }}
+                        >
+
+                            <Box
+                                sx={{
+                                    width: { xs: "70%", sm: "50%", md: "40%" },
+                                    margin: { xs: "0 auto 2rem", md: "0 2rem 0 0" }
+
+                                }}
+                            >
+                                {/* poster */}
+                                <Box
+                                sx={{
+                                    paddingTop: "140%",
+                                    ...uiConfigs.style.backgroundImage(tmdbConfigs
+                                        .posterPath(media.poster_path || media.backdrop_path))
+                                }}
+                                >
+
+                                </Box>
+                                {/* poster */}
+                                {/* info */}
+
+                                <Box
+                                sx={{
+                                    width: {xs: "100%", md: "60%"},
+                                    color: "text.primary"
+                                }}
+                                >
+                                    <Stack 
+                                    spacing={5}>
+                                        {/* title */}
+                                        <Typography
+                                        variant='h4'
+                                        fontSize={{xs: "1rem", md: "1rem", lg: "2rem"}}
+                                        fontWeight="700"
+
+                                        >
+                                            {`${media.title || media.name} ${mediaType === tmdbConfigs
+                                                .mediaType.movie ? media.release_date.split("-")[0] : media
+                                                .first_air_date.split("-")[0]}`}
+
+                                        </Typography>
+                                        {/*rate*/}
+                                        <Stack direction="row" spacing={1} alignItems="center">
+                                            <Rate value={media.vote_average}/>
+                                            <Divider orientation='vertical'/>
+                                            {genres.map((genre, index) => (
+                                                <Chip
+                                                    label={genre.name}
+                                                    variant="filled"
+                                                    color="primary"
+                                                    key={index}
+                                                >
+
+                                                </Chip>
+                                            ))}
+
+                                        </Stack>
+                                        {/*overview*/}
+                                        <Typography
+                                        variant='body1'
+                                        sx={{...uiConfigs.style.typoLines(5)}}
+                                        >
+                                            {media.overview}
+                                        </Typography>
+                                        
+                                        {/*buttons */}
+                                        <Stack direction="row" spacing={1}>
+                                            <LoadingButton 
+                                            variant='text'
+                                            sx={{
+                                                width: "max-content",
+                                                "& .MuiButton-starIcon": {marginRight: "0"}
+                                            }}
+                                            size="large"
+                                            startIcon={isFavorite ? <FavoriteIcon/> : <FavoriteBorderIcon/>}
+                                            loadingPosition="start"
+                                            loading={onRequest}
+                                            />
+                                            <Button
+                                            variant='contained'
+                                            sx={{width: "max-contend"}}
+                                            size="large"
+                                            startIcon={<PlayArrowIcon/>}
+                                            onClick={() => videoRef.current.scrollIntoView()}
+                                            >
+                                                Watch Now
+                                            </Button>
+                                            
+
+                                        </Stack>
+                                    </Stack>
+
+                                </Box>
+                                {/* info */}
+
+
+                            </Box>
+
+                        </Box>
+
+
+                    </Box>
+                </Box>
+            </>
+        ) : null
+
+
     )
 }
 
