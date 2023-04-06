445b3f6e5a1d52cd2b286cb206018e8de8aed566
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-04-06 12:15:39 +0300

    adding to review page

--- client/src/pages/ReviewList.jsx
+++ client/src/pages/ReviewList.jsx
@@ -11,7 +11,7 @@ import reviewApi from '../api/modules/reviwe.api';
 import Container from '../components/common/Container';
 import uiConfigs from '../configs/ui.config';
 import { setGlobalLoading } from '../redux/features/globalLoadingSlice';
-import { routersGen } from '../routes/routes';
+import { routesGen } from '../routes/routes';
 
 
 const ReviewItem = ({ review, onRemoved }) => {
@@ -34,25 +34,75 @@ const ReviewItem = ({ review, onRemoved }) => {
 
     return (
         <Box
-        sx={{
-            position: 'relative',
-            display: 'flex',
-            flexDirection: {xs: 'column', md: 'row'},
-            padding: 1,
-            opacity:  onRequest ? 0.6 : 1,
-            "&:hover": {backgroundColor: 'background.paper'}
-        }}>
-            <Box
             sx={{
-                width: {xs: 0, md: "10%"}
-            }}
+                position: 'relative',
+                display: 'flex',
+                flexDirection: { xs: 'column', md: 'row' },
+                padding: 1,
+                opacity: onRequest ? 0.6 : 1,
+                "&:hover": { backgroundColor: 'background.paper' }
+            }}>
+            <Box
+                sx={{
+                    width: { xs: 0, md: "10%" }
+                }}
             >
                 <Link
-                to={routersGen.mediaDetail(review.mediaType, review.mediaId)}
-                style={{color: 'unset', textDecoration: 'none'}}
+                    to={routesGen.mediaDetail(review.mediaType, review.mediaId)}
+                    style={{ color: 'unset', textDecoration: 'none' }}
                 >
+
+                    <Box
+                        sx={{
+                            paddingTop: "160%",
+                            ...uiConfigs.style.backgroundImage(tmdbConfigs.posterPath(review.mediaPoster))
+
+                        }}
+                    >
+
+                    </Box>
                 </Link>
 
+                <Box
+                    sx={{
+                        width: { xs: "100%", md: "80%" },
+                        padding: { xs: 0, md: "0 2rem" }
+                    }}>
+                    <Stack spacing={1}>
+                        <Link
+                            to={routesGen.mediaDetail(review.mediaType, review.mediaId)}
+                            style={{ color: 'unset', textDecoration: 'none' }}
+                        >
+                            <Typography
+                                variant="h6"
+                                sx={{ ...uiConfigs.style.typoLines(1, 'left') }}
+                            >
+                                {review.mediaTitle}
+                            </Typography>
+                        </Link>
+                        <Typography variant='caption'>
+                            {dayjs(review.createdAt).format("DD/MM/YYYY HH:mm:ss")}
+                        </Typography>
+                        <Typography>
+                            {review.content}
+                        </Typography>
+                    </Stack>
+                </Box>
+                <LoadingButton
+                    variant='contained'
+                    sx={{
+                        position: { xs: "relative", md: "absolute" },
+                        right: {xs: 0, md: "10px"},
+                        marginTop: {xs: 2, md: 0},
+                        width: 'max-content'
+                    }}
+                    startIcon={<DeleteIcon/>}
+                    loadingPosition='start'
+                    loading={onRequest}
+                    onClick={onRemove}
+                >
+                    remove
+                </LoadingButton>
             </Box>
 
         </Box>
@@ -60,6 +110,47 @@ const ReviewItem = ({ review, onRemoved }) => {
 }
 
 const ReviewList = () => {
+
+    const [reviews, setReviews] = useState([]);
+    const [filteredReviews, setFilteredReviews] = useState([]);
+    const [page, setPage] = useState(1);
+    const [count, setCount] = useState(0);
+
+    const dispatch = useDispatch();
+
+    const skip = 8;
+
+    useEffect(() => {
+        const getReviews = async () => {
+            dispatch(setGlobalLoading(true))
+            const { response, err } = await reviewApi.getList()
+            dispatch(setGlobalLoading(false));
+
+            if (err) toast.error(err.message)
+            if (response) {
+                setCount(response.length)
+                setReviews([...response])
+                setFilteredReviews([...response].splice(0, skip))
+            }
+        }
+
+        getReviews();
+    }, [])
+
+
+    const onLoadMore = () => {
+        setFilteredReviews([...filteredReviews, ...[...reviews].splice(page * skip, skip)]);
+        setPage(page + 1)
+    }
+
+    const onRemoved = (id) => {
+        const newReviews = [...reviews].filter(e => e.id !== id)
+        setReviews(newReviews);
+        setFilteredReviews([...newReviews].splice(0, page * skip));
+        setCount(count - 1);
+
+    }
+
     return (
         <div>ReviewList</div>
     )
