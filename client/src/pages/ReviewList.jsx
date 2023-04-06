7f5901577990880faeebccc4dac7cb94959869a0
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-04-06 12:36:12 +0300

    fixing errors and finishing review list

--- client/src/pages/ReviewList.jsx
+++ client/src/pages/ReviewList.jsx
@@ -1,5 +1,5 @@
 import { LoadingButton } from '@mui/lab';
-import { DeleteIcon } from '@mui/icons-material/Delete';
+import DeleteIcon  from '@mui/icons-material/Delete';
 import { Box, Button, Divider, Stack, Typography } from '@mui/material';
 import dayjs from 'dayjs'
 import { useDispatch } from 'react-redux';
@@ -33,79 +33,65 @@ const ReviewItem = ({ review, onRemoved }) => {
     };
 
     return (
-        <Box
-            sx={{
-                position: 'relative',
-                display: 'flex',
-                flexDirection: { xs: 'column', md: 'row' },
-                padding: 1,
-                opacity: onRequest ? 0.6 : 1,
-                "&:hover": { backgroundColor: 'background.paper' }
-            }}>
-            <Box
-                sx={{
-                    width: { xs: 0, md: "10%" }
-                }}
-            >
-                <Link
-                    to={routesGen.mediaDetail(review.mediaType, review.mediaId)}
-                    style={{ color: 'unset', textDecoration: 'none' }}
-                >
-
-                    <Box
-                        sx={{
-                            paddingTop: "160%",
-                            ...uiConfigs.style.backgroundImage(tmdbConfigs.posterPath(review.mediaPoster))
-
-                        }}
-                    >
-
-                    </Box>
-                </Link>
-
-                <Box
-                    sx={{
-                        width: { xs: "100%", md: "80%" },
-                        padding: { xs: 0, md: "0 2rem" }
-                    }}>
-                    <Stack spacing={1}>
-                        <Link
-                            to={routesGen.mediaDetail(review.mediaType, review.mediaId)}
-                            style={{ color: 'unset', textDecoration: 'none' }}
-                        >
-                            <Typography
-                                variant="h6"
-                                sx={{ ...uiConfigs.style.typoLines(1, 'left') }}
-                            >
-                                {review.mediaTitle}
-                            </Typography>
-                        </Link>
-                        <Typography variant='caption'>
-                            {dayjs(review.createdAt).format("DD/MM/YYYY HH:mm:ss")}
-                        </Typography>
-                        <Typography>
-                            {review.content}
-                        </Typography>
-                    </Stack>
-                </Box>
-                <LoadingButton
-                    variant='contained'
-                    sx={{
-                        position: { xs: "relative", md: "absolute" },
-                        right: {xs: 0, md: "10px"},
-                        marginTop: {xs: 2, md: 0},
-                        width: 'max-content'
-                    }}
-                    startIcon={<DeleteIcon/>}
-                    loadingPosition='start'
-                    loading={onRequest}
-                    onClick={onRemove}
-                >
-                    remove
-                </LoadingButton>
+        <Box sx={{
+            position: "relative",
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            padding: 1,
+            opacity: onRequest ? 0.6 : 1,
+            "&:hover": { backgroundColor: "background.paper" }
+          }}>
+            <Box sx={{ width: { xs: 0, md: "10%" } }}>
+              <Link
+                to={routesGen.mediaDetail(review.mediaType, review.mediaid)}
+                style={{ color: "unset", textDecoration: "none" }}
+              >
+                <Box sx={{
+                  paddingTop: "160%",
+                  ...uiConfigs.style.backgroundImage(tmdbConfigs.posterPath(review.mediaPoster))
+                }} />
+              </Link>
             </Box>
-
-        </Box>
+      
+            <Box sx={{
+              width: { xs: "100%", md: "80%" },
+              padding: { xs: 0, md: "0 2rem" }
+            }}>
+              <Stack spacing={1}>
+                <Link
+                  to={routesGen.mediaDetail(review.mediaType, review.mediaid)}
+                  style={{ color: "unset", textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="h6"
+                    sx={{ ...uiConfigs.style.typoLines(1, "left") }}
+                  >
+                    {review.mediaTitle}
+                  </Typography>
+                </Link>
+                <Typography variant="caption">
+                  {dayjs(review.createdAt).format("DD-MM-YYYY HH:mm:ss")}
+                </Typography>
+                <Typography>{review.content}</Typography>
+              </Stack>
+            </Box>
+      
+            <LoadingButton
+              variant="contained"
+              sx={{
+                position: { xs: "relative", md: "absolute" },
+                right: { xs: 0, md: "10px" },
+                marginTop: { xs: 2, md: 0 },
+                width: "max-content"
+              }}
+              startIcon={<DeleteIcon />}
+              loadingPosition="start"
+              loading={onRequest}
+              onClick={onRemove}
+            >
+              remove
+            </LoadingButton>
+          </Box>
     )
 }
 
@@ -152,7 +138,23 @@ const ReviewList = () => {
     }
 
     return (
-        <div>ReviewList</div>
+        <Box sx={{ ...uiConfigs.style.mainContent }}>
+      <Container header={`Your reviews (${count})`}>
+        <Stack spacing={2}>
+          {filteredReviews.map((item) => (
+            <Box key={item.id}>
+              <ReviewItem review={item} onRemoved={onRemoved} />
+              <Divider sx={{
+                display: { xs: "block", md: "none" }
+              }} />
+            </Box>
+          ))}
+          {filteredReviews.length < reviews.length && (
+            <Button onClick={onLoadMore}>load more</Button>
+          )}
+        </Stack>
+      </Container>
+    </Box>
     )
 }
 
